59cfe989e6736ed2999ee41f3fba11b8c12571ea
Yaroslav Podorvanov <63663261+YaroslavPodorvanov@users.noreply.github.com>
2026-07-03 15:42:37 +0300

    feat: init Go project (#1)

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ArtemHvozdov/food-service-establishments
+
+go 1.26.1
---
+++ internal/domain/place.go
@@ -0,0 +1,53 @@
+package domain
+
+import "time"
+
+type PlaceType struct {
+	Alias string // restaurant
+	Name  string // Ресторан
+}
+
+var (
+	Restaurant = PlaceType{
+		Alias: "restaurant",
+		Name:  "Ресторан",
+	}
+)
+
+type Place struct {
+	Alias           string   // Domain name or Instagram username, e.g. "borshch.krakow"
+	PreviousAliases []string // For redirect from old aliases to current one
+	City            City
+	Name            string
+	Address         string
+	Type            PlaceType
+	URL             string
+	MenuURL         string
+	PhotoURL        string
+	InstagramURL    string
+	GoogleMapsURL   string
+	GooglePlaceID   string
+	Location        Location
+	FoundingYear    int
+	Description     string
+	Dishes          []Dish
+	BorschPrice     string // value and currency, e.g. "50 UAH"
+	LargePortions   bool
+	HasDelivery     bool
+	DonatesToZSU    []Article
+	Articles        []Article
+	YoutubeVideos   []YoutubeVideo
+	AddedAt         time.Time
+	PromotedUntil   *time.Time
+	// TODO: add ratings TripAdvisor, Google, etc.
+}
+
+type CityPlaceGroup struct {
+	City   City
+	Places []Place
+}
+
+type CountryPlaceGroup struct {
+	Country Country
+	Cities  []CityPlaceGroup
+}
